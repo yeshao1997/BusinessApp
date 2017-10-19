158ae293f5f2b0f3f2a8561b3e19f137d84887f0
yeshao1997 <yeshao1997@outlook.com>
2017-10-20 01:59:42 +0800

    更新登录界面

--- FCSAPP/js/login.js
+++ FCSAPP/js/login.js
@@ -2,14 +2,14 @@ function login(){
 	var account = $("#account-input").val();
 	var password = $("#password-input").val();
  	
- 	var url = 'http://172.16.95.195:8080/AccountController/login';
+ 	var url = 'http://172.16.41.126:8080/AccountController/login';
       mui.ajax(url, {
         data: {
           'account': account,
           'password': password
         },
         type: "POST", 
-        timeout:10000,
+        timeout:1000,
         success: function(data) {
           console.log("result:"+JSON.stringify(data));
         }, 
@@ -19,6 +19,7 @@ function login(){
 			console.log(xhr.readyState);
 			console.log("error:"+type);
 			console.log(errorThrown);
+			mui.toast("网络错误！！！");
 		}
       });
 }
